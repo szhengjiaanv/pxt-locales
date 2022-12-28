eb87c09be08d75e4d6e4f2ca98464211ee8e85a4
szhengjiaanv <120627122+szhengjiaanv@users.noreply.github.com>
2022-12-28 12:28:24 +0800

    更新 pxt.json, main.ts, _locales/zh-CN/strings.json

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
+//% color="#610001" weight=10 
+namespace Lbit {
 
+    //% block="Turn off all LED headlights"
+    //% weight=55
+    export function closeheadlights(): void {
+        let i = 0
+    }
+
+}
