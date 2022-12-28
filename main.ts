93b804c65bf1872b98425a1412c5413a2855449a
szhengjiaanv <120627122+szhengjiaanv@users.noreply.github.com>
2022-12-28 14:23:30 +0800

    更新 main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 //% color="#610001" weight=10 
 namespace Lbit {
 
-    //% block="Turn off all LED headlights"
+    //% block="Turnoff all LED headlights"
     //% weight=55
     export function closeheadlights(): void {
         let i = 0
